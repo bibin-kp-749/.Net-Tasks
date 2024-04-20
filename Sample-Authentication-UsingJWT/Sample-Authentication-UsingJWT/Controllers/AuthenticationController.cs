985518a08a8ed868b9cbf8221a417b2d8ff63215
BIBIN KP <136179947+bibin-kp-749@users.noreply.github.com>
2024-04-20 20:25:42 +0530

    Add files via upload

---
+++ Sample-Authentication-UsingJWT/Sample-Authentication-UsingJWT/Controllers/AuthenticationController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Sample_Authentication_UsingJWT.Model;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Sample_Authentication_UsingJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        IConfiguration _configuration;
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Auth([FromBody] User user)
+        {
+            IActionResult response=Unauthorized();
+            if(user!=null)
+            {
+                if(user.UserName.Equals("zack@gmail.com")&&user.Password.Equals("12345"))
+                {
+                    var issuer = _configuration["Jwt:Issuer"];
+                    var audience = _configuration["Jwt:Audience"];
+                    var key = Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]);
+                    var signingCredentials = new SigningCredentials(
+                        new SymmetricSecurityKey(key),
+                        SecurityAlgorithms.HmacSha512Signature
+                        );
+                    var subject = new ClaimsIdentity(new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub,user.UserName),
+                        new Claim(JwtRegisteredClaimNames.Email,user.UserName),
+                    });
+                    var expires=DateTime.UtcNow.AddMinutes(1);
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = subject,
+                        Expires = expires,
+                        Issuer = issuer,
+                        Audience = audience,
+                        SigningCredentials = signingCredentials
+                    };
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var token = tokenHandler.CreateToken(tokenDescriptor);
+                    var jwtToken = tokenHandler.WriteToken(token);
+
+                    return Ok(jwtToken);
+                }
+            }
+            return response;
+        }
+    }
+}
---
+++ Sample-Authentication-UsingJWT/Sample-Authentication-UsingJWT/Model/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Sample_Authentication_UsingJWT.Model
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public User(string UserName, string Password)
+        {
+            this.UserName=UserName;
+            this.Password = Password;
+        }
+    }
+}
