41b0e84960d7118d4052c80d03c0ac0a37979fb5
BIBIN KP <136179947+bibin-kp-749@users.noreply.github.com>
2024-05-03 16:37:05 +0530

    Add files via upload

---
+++ SampleConsole_Ado_net/SampleConsole_Ado_net/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+namespace ConsoleSampleado_net
+{
+    class Program
+    {
+    public static void Main(string[] args)
+    {
+            //created an innstance of Program class
+            Program pg = new Program();
+            pg.GetResult();
+    }
+        private void GetResult()
+        {
+            //estabished Connection to the sql server
+            SqlConnection _Con = new SqlConnection("data source = DESKTOP-QJT086F ; database = practical_section ; integrated security = SSPI ; TrustServerCertificate = true");
+            //Defined the command for performing
+            SqlCommand _Command = new SqlCommand("select * from Students", _Con);
+            try
+            {
+                //checking the Connection is closed or not 
+                if (_Con.State == ConnectionState.Closed)
+                    _Con.Open(); //If it is true open the connection
+                SqlDataReader rd = _Command.ExecuteReader();//executed the command
+                while (rd.Read()) //consoled the result by using loop
+                {
+                    Console.WriteLine(rd["student_name"]+" -->  "+ rd["marks"]);
+                }
+            }catch (Exception ex) //handled the exception
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally //closed the connection  
+            {
+                if (_Con.State == ConnectionState.Open)
+                    _Con.Close(); 
+            }
+        }
+
+    }
+}
