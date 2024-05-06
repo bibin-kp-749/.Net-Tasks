cc52322125d8636162b5cde8b411681a684d8ae0
BIBIN KP <136179947+bibin-kp-749@users.noreply.github.com>
2024-05-06 20:54:21 +0530

    Add files via upload

---
+++ DatasetModifyingin_ADO.net/DatasetModifyingin_ADO.net/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
+
+namespace DatasetModifyingAdo_Net
+{
+    class Program
+    { 
+        public static void Main(string[] args)
+        {  
+            string _connectionString = "data source = DESKTOP-QJT086F ; database = Bibin ; integrated security = SSPI ; TrustServerCertificate = true";
+            using(SqlConnection _con=new SqlConnection(_connectionString))
+            {
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("select * from Books", _con);
+                DataSet ds = new DataSet();
+                dataAdapter.Fill(ds);
+                foreach(DataRow row in ds.Tables[0].Rows)
+                {
+                    Console.WriteLine(row[0]);
+                }
+            }
+        }
+        //Method to Insert data
+        private void InsertRecord(string bookname,int Auther_id)
+        {
+            
+        }
+    }
+}
