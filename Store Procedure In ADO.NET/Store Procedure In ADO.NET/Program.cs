7717c2f17b58144bb8f3af89663ef7957d818090
BIBIN KP <136179947+bibin-kp-749@users.noreply.github.com>
2024-05-10 23:12:45 +0530

    Update Program.cs

--- Store Procedure In ADO.NET/Store Procedure In ADO.NET/Program.cs
+++ Store Procedure In ADO.NET/Store Procedure In ADO.NET/Program.cs
@@ -5,48 +5,60 @@ namespace StoreProcedureInADO_NET
 {
     class Program
     {
+        //Defined connetionString
         private readonly string connectionstr = "data source = DESKTOP-QJT086F ; database = master ; integrated security = SSPI ; TrustServerCertificate = true";
         static void Main(string[] args)
         {
+            //Create an instance of Program
             Program program = new Program();
             program.SP_WithoutInput_and_Output();
             program.SP_WithInput_and_Without_Output(103);
             program.SP_WithInput_and_Output(103);
         }
+        //Method for Calling StoreProcedure WithoutInput&output
         private void SP_WithoutInput_and_Output()
         {
             try
             {
+                //Creating Sql connection object using ConnectionString
                 using (SqlConnection _con = new SqlConnection(connectionstr))
                 {
+                    //Checikng the connection is opened or not
                     if (_con.State == ConnectionState.Closed)
                         _con.Open();
+                    //Implementing Storeprocedure using connection object
                     using (SqlCommand _cmd = new SqlCommand("GetAllData",_con))
                     {
+                        //Specify the storeProcedure
                         _cmd.CommandType = CommandType.StoredProcedure;
                         SqlDataReader rd = _cmd.ExecuteReader();
                         while (rd.Read())
                             Console.WriteLine($"{rd[0]}    {rd[1]}    {rd[2]}");
                     }
                 }
-            } catch (SqlException ex)
+            } catch (SqlException ex) //Catching sql exception
             {
                 Console.WriteLine("Sql exception occured",ex.Message);
-            }catch (Exception ex)
+            }catch (Exception ex) //Catching exception
             {
                 Console.WriteLine("Exception occured", ex.StackTrace);
             }
         }
+        //Method for Calling StoreProcedure With Input& Without output
         private void SP_WithInput_and_Without_Output(int Id)
         {
             try
             {
+                //Creating Sql connection object using ConnectionString
                 using(SqlConnection _con = new SqlConnection(connectionstr))
                 {
+                    //Checikng the connection is opened or not
                     if (_con.State == ConnectionState.Closed)
                         _con.Open();
+                    //Implementing Storeprocedure using connection object
                     using(SqlCommand _cmd = new SqlCommand("View_databy_id", _con))
                     {
+                        //Specify the storeProcedure
                         _cmd.CommandType = CommandType.StoredProcedure;
                         _cmd.Parameters.AddWithValue("@id", Id);
                         SqlDataReader dr=_cmd.ExecuteReader();
@@ -55,24 +67,29 @@ namespace StoreProcedureInADO_NET
 
                     }
                 }
-            }catch(SqlException ex)
+            }catch(SqlException ex) //Catching sql exception
             {
                 Console.WriteLine("Sql exception occure ",ex.Message);
-            }catch(Exception ex)
+            }catch(Exception ex) //Catching exception
             {
                 Console.WriteLine("Exception occure ", ex.Message);
             }
         }
+        //Method for Calling StoreProcedure With Input&output
         private void SP_WithInput_and_Output(int Id)
         {
             try
             {
+                //Creating Sql connection object using ConnectionString
                 using (SqlConnection _con = new SqlConnection(connectionstr))
                 {
+                    //Checikng the connection is opened or not
                     if (_con.State == ConnectionState.Closed)
                         _con.Open();
+                    //Implementing Storeprocedure using connection object
                     using (SqlCommand _cmd = new SqlCommand("Return_databyId", _con))
                     {
+                        //Specify the storeProcedure
                         _cmd.CommandType = CommandType.StoredProcedure;
                         _cmd.Parameters.AddWithValue("@id", Id);
                         SqlParameter output = new SqlParameter("@kk", SqlDbType.Int)
@@ -84,10 +101,10 @@ namespace StoreProcedureInADO_NET
                         Console.WriteLine((int)_cmd.Parameters["@kk"].Value);
                     }
                 }
-            } catch(SqlException ex)
+            } catch(SqlException ex) //Catching sql exception
             {
                 Console.WriteLine("Sql exception occure ", ex.Message);
-            }catch (Exception ex)
+            }catch (Exception ex) //Catching exception
             {
                 Console.WriteLine("Exception occure ", ex.Message);
             }
