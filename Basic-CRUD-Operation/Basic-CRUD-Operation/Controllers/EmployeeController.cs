a005f27f71732e7d134a4f212443c64b87aa1216
BIBIN KP <136179947+bibin-kp-749@users.noreply.github.com>
2024-05-11 22:58:15 +0530

    Update EmployeeController.cs

--- Basic-CRUD-Operation/Basic-CRUD-Operation/Controllers/EmployeeController.cs
+++ Basic-CRUD-Operation/Basic-CRUD-Operation/Controllers/EmployeeController.cs
@@ -10,15 +10,19 @@ namespace Basic_CRUD_Operation.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        //Created an instance of Employee service class for performing CRUD operations
         Employees employee=new Employees();
+        //ActionMethod for getting Allemployee
         [HttpGet]
         public ActionResult GetDetails()
         {
             return Ok(employee.GetEmployee());
         }
+        //ActionMethod for addig new employee
         [HttpPost]
         public ActionResult PostDetails(Employee newitem)
         {
+            //Checking EmployeeId is Zero
             if (newitem.EmployeeId == 0)
             {
                 return BadRequest("the id doesnot contain Zero");
@@ -30,13 +34,14 @@ namespace Basic_CRUD_Operation.Controllers
             }
         }
         
-       
+        //ActionMethod for Updating employee 
         [HttpPut]
         public ActionResult EditEmployee(Employee newitem)
         {
             employee.EditEmployee(newitem);
             return Ok(employee.employees);
         }
+        //ActionMethod for deleting employee 
         [HttpDelete]
         public ActionResult DeleteEmployee(int id) 
         {
